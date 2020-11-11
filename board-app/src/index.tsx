b024630606563bf4c1059b90f4ca7c4b351b2385
Harry Black <HarryB@hq.trayport.com>
2020-11-11 15:37:23 +0000

    Update import in index

--- board-app/src/index.tsx
+++ board-app/src/index.tsx
@@ -1,22 +1,24 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { createGlobalStyle } from 'styled-components'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
 // Import main Board component
-import { Board } from './components/Board'
+import { Board } from "./components/boardComponents/Board";
 
 // Use createGlobalStyle to change the background of 'body' element
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #2053D6;
   }
-`
+`;
 
 // Create component for the page
-const Page = () => (<>
-  <Board />
-  <GlobalStyle />
-</>)
+const Page = () => (
+  <>
+    <Board />
+    <GlobalStyle />
+  </>
+);
 
 // Render the page into DOM
-ReactDOM.render(<Page />, document.getElementById('root'))
+ReactDOM.render(<Page />, document.getElementById("root"));
